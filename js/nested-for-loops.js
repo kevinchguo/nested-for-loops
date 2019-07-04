d7b16bd3ad8338ce694fc8348860afd2c535bb9b
Kevin Guo <Kevin@Kevins-Air.home>
2019-07-04 03:48:58 -1000

    cracking password example

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -6,10 +6,22 @@
  */
 function nestedForLoops (depth, width = depth) {
   let result = "\n";
-
+  let empStr = '';
   // Write code here
-
+  
+  empStr += result
+  for (let x = 0; x < depth; x++) {
+    for (let y = 0; y < width; y++) {
+      empStr += "{x:" + y + ", y:" + x + "}"
+      if (y < width - 1) {
+        empStr += ", "
+      }
+    }
+    empStr += result
+  }
+  return empStr
 };
 
 // To see your console output outside the tests add function calls here.
-// console.log(nestedForLoops(4));
+console.log(nestedForLoops(3));
+console.log(nestedForLoops(10,5))
